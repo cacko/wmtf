91d6424275ea1e5d7e28ddfd611f4e178edd7a5c
cacko <alex@cacko.net>
2022-10-30 23:59:45 +0000

    Whee.

--- t.py
+++ t.py
@@ -1,6 +1,19 @@
 from bs4 import BeautifulSoup, element
 from pathlib import Path
 from pprint import pprint
+from collections import namedtuple
+from urllib.parse import parse_qs, urlparse
+
+def extract_id_from_a(el: element.Tag):
+    if el.name != "a":
+        raise ValueError
+    url = urlparse(el.attrs.get("href", ""))
+    query = url.query
+    itemId = parse_qs(query).get("itemId", [])
+    return int(itemId[0])
+
+
+TaskRow = namedtuple('TaskRow', "id,clock,summary,parent,group,priority,assignee,jira,update,create,dealline,bus_value,sched,work,sales_task,sales_pipeline,open")
 
 f = Path(".") / "tasks.html"
 
@@ -14,5 +27,12 @@ with f.open("rb") as fp:
             match(type(td)):
                 case element.Tag:
                     item.append(td)
-        items.append(item)
-    print(items[6])
+        try:
+            items.append(TaskRow(*item))
+        except TypeError:
+            pass
+    tasks = []
+    for t in items:
+        id = extract_id_from_a(t.id.find("a"))
+        summary = t.summary.get_text(strip=True)
+        print(id, summary)
---
+++ wmtf/wm/html/__init__.py
@@ -0,0 +1,10 @@
+from bs4 import element
+from urllib.parse import parse_qs, urlparse
+
+def extract_id_from_a(el: element.Tag):
+    if el.name != "a":
+        raise ValueError
+    url = urlparse(el.attrs.get("href", ""))
+    query = url.query
+    itemId = parse_qs(query).get("itemId", [])
+    return int(itemId[0])
