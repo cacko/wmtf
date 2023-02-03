345eb497791165d930dd46462dc8cdfb0a0d2936
cacko <alex@cacko.net>
2023-02-03 12:54:57 +0000

    Who Let the Bugs Out??

--- setup.py
+++ setup.py
@@ -42,7 +42,7 @@ setup(
         "python-dateutil>=2.8.2",
         "pytz-deprecation-shim>=0.1.0.post0",
         "questionary>=1.10.0",
-        "textual>=0.11.0",
+        "textual>=0.10.1",
         "tzlocal>=4.2",
         "click>=8.1.3",
         "jira==3.4.1",
